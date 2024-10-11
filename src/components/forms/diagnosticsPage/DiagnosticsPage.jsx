9f14f12b024eccef14e3a5bfd7a420dd9de18868
ImpulsVL <stiepochkinv@mail.ru>
2024-10-11 05:00:02 +0500

    создал печать

--- src/components/forms/diagnosticsPage/DiagnosticsPage.jsx
+++ src/components/forms/diagnosticsPage/DiagnosticsPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './DiagnosticsPage.scss';
 import PDFIcon from '../icons/pdf.svg';
 import Header from '../../header/header';
@@ -22,9 +22,9 @@ function DiagnosticsPage() {
         e.preventDefault();
         let temp = 0;
 
-         Object.values(KnownRisksFactorValue).map((item) => {
-             temp += Number(item);
-         });
+        Object.values(KnownRisksFactorValue).map((item) => {
+            temp += Number(item);
+        });
 
         setResult(temp);
         console.log(temp);
@@ -37,7 +37,7 @@ function DiagnosticsPage() {
         let interpretationRisk = result < 12
             ? "Низкая вероятность наличия послеродовой депрессии"
             : "Высокая вероятность наличия послеродовой депрессии. Для подтверждения диагноза и разработки плана лечения необходима тщательная клиническая оценка медицинским работником";
-        if(KnownRisksFactorValue[10] > 1) {
+        if (KnownRisksFactorValue[10] > 1) {
             interpretationRisk = "Высокая вероятность наличия послеродовой депрессии. За вопрос №10 получено " + KnownRisksFactorValue[10] + " балла, рекомендуется консультация психиатра";
         }
         setRisk(interpretationRisk);
@@ -45,8 +45,25 @@ function DiagnosticsPage() {
     const handleRisksChange = (e) => {
         const { name } = e.target;
         // setKnownRisksCriteria(prev => ({ ...prev, [name]: checked }));
-        setKnownRisksFactorValue(val => ({ ...val, [name]: Number(e.target.value)}))
+        setKnownRisksFactorValue(val => ({ ...val, [name]: Number(e.target.value) }))
         console.log(e.target.value);
+    };
+
+    const handlePrint = () => {
+        const printContent = document.querySelectorAll('.printableArea');
+        const printWindow = window.open('', '', 'height=600,width=800');
+        printWindow.document.write('<html><head><title></title>');
+        printWindow.document.write('<link rel="stylesheet" type="text/css" href="path/to/your/styles.css">'); // Убедитесь, что вы подключаете стили
+        printWindow.document.write('</head><body >');
+
+        // Собираем содержимое всех элементов с классом printableArea
+        printContent.forEach(item => {
+            printWindow.document.write(item.innerHTML);
+        });
+
+        printWindow.document.write('</body></html>');
+        printWindow.document.close();
+        printWindow.print();
     };
 
     return (
@@ -60,29 +77,37 @@ function DiagnosticsPage() {
                 </div>
                 <div className='forms_block'>
                     <div className='question-list'>
-                        <div className="question-list-item">
-                            <h3 className="question-list-header">1. Насколько обильны Ваши кровотечения?</h3>
-                            <p>— Меняете ли Вы санитарные средства ночью/просыпаетесь ли для того, чтобы сменить прокладки?</p>
-                            <p>— Во время дней наиболее обильного кровотечения промокают ли полностью Ваши средства защиты после 2 ч?</p>
+                        <div className="question-list-item printableArea">
+                            <div className="question-list-item">
+                                <h3 className="question-list-header">1. Насколько обильны Ваши кровотечения?</h3>
+                                <p>— Меняете ли Вы санитарные средства ночью/просыпаетесь ли для того, чтобы сменить прокладки?</p>
+                                <p>— Во время дней наиболее обильного кровотечения промокают ли полностью Ваши средства защиты после 2 ч?</p>
+                            </div>
                         </div>
-                        <div className="question-list-item">
-                            <h3 className="question-list-header">2. Влияет ли это на Ваше физическое состояние?</h3>
-                            <p>— Наблюдаете ли вы выделение больших сгустков во время менструации?</p>
-                            <p>— Была ли у вас выявлена анемия или дефицит железа?</p>
+                        <div className="question-list-item printableArea">
+                            <div className="question-list-item">
+                                <h3 className="question-list-header">2. Влияет ли это на Ваше физическое состояние?</h3>
+                                <p>— Наблюдаете ли вы выделение больших сгустков во время менструации?</p>
+                                <p>— Была ли у вас выявлена анемия или дефицит железа?</p>
+                            </div>
                         </div>
-                        <div className="question-list-item">
-                            <h3 className="question-list-header">3. Влияет ли это на качество жизни?</h3>
-                            <p>— Вынуждены ли Вы специально организовывать Вашу социальную жизнь в эти дни?</p>
-                            <p>— Беспокоитесь ли Вы о неприятных моментах, связанных с кровотечением?</p>
+                        <div className="question-list-item printableArea">
+                            <div className="question-list-item">
+                                <h3 className="question-list-header">3. Влияет ли это на качество жизни?</h3>
+                                <p>— Вынуждены ли Вы специально организовывать Вашу социальную жизнь в эти дни?</p>
+                                <p>— Беспокоитесь ли Вы о неприятных моментах, связанных с кровотечением?</p>
+                            </div>
                         </div>
-                        <button>
+                        <button className='printer_button' onClick={handlePrint}>
                             Распечатать
                         </button>
                     </div>
                 </div>
                 <div className="forms_block">
-                    <h3 className="question-list-header">Интерпретация</h3>
-                    <p><color>Хотя бы 1 положительный ответ</color> позволяет предположить наличие обильных менструаций</p>
+                    <div className="question-list-item printableArea">
+                        <h3 className="question-list-header">Интерпретация</h3>
+                        <p><color>Хотя бы 1 положительный ответ</color> позволяет предположить наличие обильных менструаций</p>
+                    </div>
                 </div>
             </div>
         </div>
